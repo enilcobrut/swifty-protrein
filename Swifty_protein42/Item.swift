d9aa7f75e41ca19303a75b91dc03ed6ce25515dc
Celine Junker <celinya53@gmail.com>
2024-11-24 13:37:24 +0100

    Initial Commit

---
+++ Swifty_protein42/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Swifty_protein42
+//
+//  Created by Celine Junker on 24/11/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
